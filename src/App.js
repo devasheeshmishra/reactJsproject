b8d6519813c21c1d9136e5cf149eb9c8291c1439
devasheeshmishra <devasheeshmishra@gmail.com>
2021-09-08 20:51:43 +0530

    chnge

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+  }
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        this.setState({ posts: response.data })
+      })
+      .catch(error => {
+        console.log(error);
+
+      })
+  }
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        {
+          posts.length ?
+            posts.map(data => <div key={data.id}>{data.title}</div>) : null
+        }
+      </div>
+    )
+  }
+}
+export default App
